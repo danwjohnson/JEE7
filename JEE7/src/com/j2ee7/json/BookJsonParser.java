d9960a7d692c0ded2a18feee4c963d0c809c0371
Dan Johnson <djboy71@gmail.com>
2014-02-11 10:32:11 -0600

    Added method parseAllBookTitles()

--- JEE7/src/com/j2ee7/json/BookJsonParser.java
+++ JEE7/src/com/j2ee7/json/BookJsonParser.java
@@ -2,6 +2,8 @@ package com.j2ee7.json;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.json.Json;
 import javax.json.stream.JsonParser;
@@ -27,6 +29,23 @@ public class BookJsonParser {
 		String title = parseBookAndGetTitle(parser);
 		
 		System.out.println ("Book title: " + title);
+		System.out.println("\n\nStarting to parse all book titles");
+		
+		// reset the parser
+		parser.close();
+		parser = createParser();
+		
+		// Begin parsing again
+		ArrayList<String> titles = new ArrayList<String>();
+		titles = parseAllBookTitles(parser);
+		
+		Iterator<String> iter = titles.iterator();
+		
+		while(iter.hasNext()) {
+			
+			System.out.println("Book: " + iter.next());
+			
+		} // end while loop
 		
 	} // end parseJson()
 	
@@ -70,6 +89,39 @@ public class BookJsonParser {
 		
 	} // end parseBookAndGetTitle()
 	
+	
+	private ArrayList<String> parseAllBookTitles(JsonParser parser) {
+		
+		ArrayList<String> titles = new ArrayList<String>();
+		
+		try {
+			
+			while (parser.hasNext()) {
+				
+				JsonParser.Event event = parser.next();
+				while (parser.hasNext() && !(event.equals(JsonParser.Event.KEY_NAME)
+						&& parser.getString().matches("title"))) {
+					
+					event = parser.next();
+					
+				} // end inner while loop
+				
+				if (event.equals(JsonParser.Event.KEY_NAME) &&
+						parser.getString().matches("title")) {
+					
+					parser.next();
+					titles.add(parser.getString());
+					
+				} // end if
+				
+			} // end while loop
+			
+		} // end try
+		catch (Exception ex) {}	// end catch
+		
+		return titles;
+		
+	} // end parseAllBookTitles()
 	
 	/**
 	 * Create the JSON parser
